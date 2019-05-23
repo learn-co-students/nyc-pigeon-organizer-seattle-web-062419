90d7cbfd413fccacc45be85e9ede1b296c77ac43
Xinran Che <gracexinran@gmail.com>
2019-05-23 00:17:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |k, v|
+    v.each do |key,value|
+      value.each do |name|
+        if !list[name]
+          list[name] = {}
+        end
+        if !list[name][k]
+          list[name][k] = [key.to_s]
+        else
+          list[name][k] << key.to_s
+        end
+      end
+    end
+  end
+  list
+
 end
